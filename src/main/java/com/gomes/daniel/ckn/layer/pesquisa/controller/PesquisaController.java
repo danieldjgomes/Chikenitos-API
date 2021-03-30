883bba0b4361955a0a56542aa813022de7ae1b78
danieldjgomes <daniel.djgomes@outlook.com>
2021-03-29 21:41:13 -0300

    CKN API 1.0.8

--- src/main/java/com/gomes/daniel/ckn/layer/pesquisa/controller/PesquisaController.java
+++ src/main/java/com/gomes/daniel/ckn/layer/pesquisa/controller/PesquisaController.java
@@ -1,25 +1,19 @@
-package com.gomes.daniel.ckn.layer.pesquisa.controller;
+                                                                                                                                                       package com.gomes.daniel.ckn.layer.pesquisa.controller;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gomes.daniel.ckn.layer.pesquisa.domain.exception.EntidadeNãoEncontradaException;
 import com.gomes.daniel.ckn.layer.pesquisa.domain.model.Pesquisa;
 import com.gomes.daniel.ckn.layer.pesquisa.infrastructure.root.PesquisaRoot;
-import com.gomes.daniel.ckn.layer.relatorio.domain.model.Relatorio;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/pesquisas")
 public class PesquisaController {
 	
@@ -33,33 +27,29 @@ public class PesquisaController {
 			return ResponseEntity.ok(pesquisas);
 		return ResponseEntity.notFound().build();
 	}
-	@GetMapping("/{pesquisaId}")
-	public ResponseEntity<Pesquisa> buscar(@PathVariable long pesquisaId){
-		Pesquisa pesquisa = pesquisaRoot.buscar(pesquisaId);
-		
-		if (pesquisa != null) {
-			return ResponseEntity.ok(pesquisa);
-		}
-		return ResponseEntity.notFound().build();
-	}
-	
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public Pesquisa salvar(@RequestBody Pesquisa pesquisa){
-		return pesquisaRoot.salvar(pesquisa);
-	}
-	
-	
-	@DeleteMapping(("/{pesquisaId}"))
-	public ResponseEntity<Pesquisa> remover(@PathVariable long pesquisaId) {
-		try {
-			pesquisaRoot.remover(pesquisaId);
-		}
-		catch(EntidadeNãoEncontradaException e) {
-			return ResponseEntity.notFound().build();
-		}
-		return ResponseEntity.noContent().build();
-	}
+//	@GetMapping("/{pesquisaId}")
+//	public ResponseEntity<Pesquisa> buscar(@PathVariable long pesquisaId){
+//		Pesquisa pesquisa = pesquisaRoot.buscar(pesquisaId);
+//		if (pesquisa != null) {
+//			return ResponseEntity.ok(pesquisa);
+//		}
+//		return ResponseEntity.notFound().build();
+//	}
+//	@PostMapping
+//	@ResponseStatus(HttpStatus.CREATED)
+//	public Pesquisa salvar(@RequestBody Pesquisa pesquisa){
+//		return pesquisaRoot.salvar(pesquisa);
+//	}
+//	@DeleteMapping(("/{pesquisaId}"))
+//	public ResponseEntity<Pesquisa> remover(@PathVariable long pesquisaId) {
+//		try {
+//			pesquisaRoot.remover(pesquisaId);
+//		}
+//		catch(EntidadeNãoEncontradaException e) {
+//			return ResponseEntity.notFound().build();
+//		}
+//		return ResponseEntity.noContent().build();
+//	}
 	
 	
 
