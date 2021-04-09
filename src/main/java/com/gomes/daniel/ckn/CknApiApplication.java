ce1b6526dadb6f99fc9d24ed07f56eec79b44991
danieldjgomes <daniel.djgomes@outlook.com>
2021-04-09 01:05:29 -0300

    CKN API 1.1.2

--- src/main/java/com/gomes/daniel/ckn/CknApiApplication.java
+++ src/main/java/com/gomes/daniel/ckn/CknApiApplication.java
@@ -1,13 +1,24 @@
 package com.gomes.daniel.ckn;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.gomes.daniel.ckn.layer.relatorio.service.RelatorioService;
+
 @SpringBootApplication
 public class CknApiApplication {
-
+	
+	@Autowired
+	RelatorioService relatorioService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CknApiApplication.class, args);
+		relatorioService.GeraRelatorioDiario();
+		
+		
+		
+	
 		
 		
 
